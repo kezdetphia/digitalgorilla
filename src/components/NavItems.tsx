6ce1acf54b1967c491669d57aacd25497e5bdbf5
kezdetphia <fehermark88@gmail.com>
2023-12-13 17:05:20 -0800

    add closing dropdown on esc button

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,18 +1,31 @@
 "use client";
 
 import { PRODUCT_CATERGORIES } from "@/config";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import NavItem from "./NavItem";
 import { useOnClickOutside } from "./hooks/use-on-click-outside";
 
 export default function NavItems() {
   const [activeIndex, setActiveIndex] = useState<null | number>(null);
 
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') {
+        setActiveIndex(null);
+      }
+    };
+    document.addEventListener('keydown', handler)
+
+    return()=>{
+      document.removeEventListener('keydown', handler)
+    }
+  }, []);
+
   const isAnyOpen = activeIndex !== null;
 
-  const navRef = useRef<HTMLDivElement | null>(null)
+  const navRef = useRef<HTMLDivElement | null>(null);
 
-  useOnClickOutside(navRef, ()=>setActiveIndex(null))
+  useOnClickOutside(navRef, () => setActiveIndex(null));
 
   return (
     <div className="flex gap-4 h-full" ref={navRef}>
