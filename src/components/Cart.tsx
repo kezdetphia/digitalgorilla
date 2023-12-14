543c2c3ef5a668a9feab32b2a5f00a0483321d5d
kezdetphia <fehermark88@gmail.com>
2023-12-13 17:39:36 -0800

    cart click reacts

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,13 +1,24 @@
 "use client";
 import { LucideShoppingCart } from "lucide-react";
-import { Sheet, SheetTrigger } from "./ui/sheet";
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
 
 export default function Cart() {
   return (
     <Sheet>
-      <SheetTrigger className="group">
-        <LucideShoppingCart className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500" />
+      <SheetTrigger className="group -m-2 flex items-center p-2">
+        <LucideShoppingCart
+          className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
+          aria-hidden="true"
+        />
+        <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
+          0
+        </span>
       </SheetTrigger>
+      <SheetContent className="flex w-full flex-col pr-0 sm:max-w-lg">
+        <SheetHeader className="space-y-2.5 pr-6">
+          <SheetTitle>Cart (0)</SheetTitle>
+        </SheetHeader>
+      </SheetContent>
     </Sheet>
   );
 }
