ab71f0828954fd2918960c7f3db546eb8c44c340
kezdetphia <fehermark88@gmail.com>
2023-12-13 19:17:54 -0800

    cart design, empty cart design

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,8 +1,24 @@
 "use client";
 import { LucideShoppingCart } from "lucide-react";
-import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
+import {
+  Sheet,
+  SheetContent,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { Separator } from "./ui/separator";
+import { formatPrice } from "@/lib/utils";
+import { buttonVariants } from "./ui/button";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Cart() {
+  const itemCount = 0;
+
+  const fee = 1;
+
   return (
     <Sheet>
       <SheetTrigger className="group -m-2 flex items-center p-2">
@@ -18,6 +34,67 @@ export default function Cart() {
         <SheetHeader className="space-y-2.5 pr-6">
           <SheetTitle>Cart (0)</SheetTitle>
         </SheetHeader>
+        {itemCount > 0 ? (
+          <>
+            <div className="flex w-full flex-col pr-6">
+              {/* TODO: cart logic */}
+              cart items
+            </div>
+            <div className="space-y-4 pr-6">
+              <Separator />
+              <div className="space-y-1 5 text-sm">
+                <div className="flex">
+                  <span className="flex-1">Shipping</span>
+                  <span className="flex-1">Free</span>
+                </div>
+                <div className="flex">
+                  <span className="flex-1">Transaction Fee</span>
+                  <span className="flex-1">{formatPrice(fee)}</span>
+                  {/* TODO: make fee 0 if the cart is empty */}
+                </div>
+                <div className="flex">
+                  <span className="flex-1">Total</span>
+                  <span className="flex-1">{formatPrice(fee)}</span>
+                </div>
+              </div>
+              <SheetFooter>
+                <SheetTrigger asChild>
+                  <Link
+                    href="/cart"
+                    className={buttonVariants({
+                      className: "w-full",
+                    })}
+                  >
+                    Continue to Checkout
+                  </Link>
+                </SheetTrigger>
+              </SheetFooter>
+            </div>
+          </>
+        ) : (
+          <div className="flex h-full flex-col items-center justify-center space-y-1">
+            <div
+              aria-hidden="true"
+              className="relative mb-4 h-60 w-60 text-muted-foreground"
+            >
+              <Image
+                src="/gorilla-empty-cart.png"
+                fill
+                alt="empty shopping cart image"
+              />
+            </div>
+            <div className="text-xl font-semibold">Your cart is empty</div>
+            <SheetTrigger asChild>
+              <Link href='/products' className={buttonVariants({
+                variant: 'link',
+                size: 'sm',
+                className: 'text-sm text-muted-foreground'
+              })}>
+                Add items to your cart to checkout
+              </Link>
+            </SheetTrigger>
+          </div>
+        )}
       </SheetContent>
     </Sheet>
   );
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,27 @@
 import { type ClassValue, clsx } from "clsx"
+import { stringify } from "querystring"
 import { twMerge } from "tailwind-merge"
  
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export function formatPrice(
+  price: number | string,
+  options: {
+    currency?: 'USD' | 'EUR' | 'GBP' | 'BDT',
+    notation? : Intl.NumberFormatOptions['notation']
+  }={}
+  ){
+    const {currency = 'USD', notation='compact'} = options
+
+    const numbericPrice = typeof price === 'string' ? parseFloat(price) : price
+
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency,
+      notation,
+      maximumFractionDigits:2
+    }).format(numbericPrice)
+  }
