590d69a9c7f1603636592f3691ddeb9a3abb918d
kezdetphia <fehermark88@gmail.com>
2023-12-15 15:56:56 -0800

    verify email page

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -2,9 +2,20 @@ import dotenv from "dotenv";
 import path from "path";
 import type { InitOptions } from "payload/config";
 import payload, { Payload } from "payload";
+import nodemailer from 'nodemailer'
 
 dotenv.config({
   path: path.resolve(__dirname, "../.env"),
+});
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.resend.com",
+  secure: true,
+  port: 465,
+  auth: {
+    user: "resend",
+    pass: process.env.RESEND_API_KEY,
+  },
 });
 
 let cached = (global as any).payload;
@@ -29,6 +40,11 @@ export const getPayLoadCLient = async ({ initOptions }: Args = {}):Promise<Paylo
 
   if (!cached.promise) {
     cached.promise = payload.init({
+      email: {
+        transport: transporter,
+        fromAddress: "fehermark88@gmail.com",
+        fromName: "DigitalGorilla",
+      },
       secret: process.env.PAYLOAD_SECRET,
       local: initOptions?.express ? false : true,
       ...(initOptions || {}),
