d91be2147b5d75f1e7c5bb595549e1fd1a97aaf0
kezdetphia <fehermark88@gmail.com>
2023-12-17 20:57:17 -0800

    fix imageslider classes

--- src/components/ImageSider.tsx
+++ src/components/ImageSider.tsx
@@ -1,20 +1,25 @@
+'use client'
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
-import 'swiper/css'
-import 'swiper/css/pagination'
-
+import "swiper/css";
+import "swiper/css/pagination";
+import type SwiperType from 'swiper'
+import { useState } from "react";
 
 interface ImageSliderProps {
   urls: string[];
 }
 export default function ImageSlider({ urls }: ImageSliderProps) {
+  const [swiper, setSwiper] = useState<null | SwiperType>(null)
+  const [activeIndex, setActiveIndex]= useState(0)
+
   const activeStyles =
     "acrtive:scale-[0.79] grid opacity-100 hover:scale-105 absolute top-1/2 -translate-y-1/2 aspect-square h-8 w-8 z-50 place-items-center rounded-full border-2 bg-white border-zin-300";
   const inactiveStyles = "hidden text-gray-400";
 
   return (
     <div className="group relative bg-zinc-100 aspect-square overflow-hidden rounded-xl">
-      <div className="aboslute z-10 inset-0 opacity-0 group-hover:opacity-100 transition">
+      <div className="absolute z-10 inset-0 opacity-0 group-hover:opacity-100 transition">
         <button></button>
         <button></button>
       </div>
--- src/components/ProductListing.tsx
+++ src/components/ProductListing.tsx
@@ -44,7 +44,7 @@ export default function ProductListing({
         })}
         href={`/products/${product.id}`}
       >
-        <div className="flex flex-col w-full">
+        <div className="flex flex-col  w-full">
           
           <ImageSlider urls={validUrls} />
           <h3 className="mt-4 font-medium text-sm text-gray-700">
