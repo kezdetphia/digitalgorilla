f0a65264376eb142b9b7e5110e4a56d7b56d0ba6
kezdetphia <fehermark88@gmail.com>
2023-12-22 10:16:36 -0800

    debug productreel, adding cart page,

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
 
 export default function Page() {
-  return (
-    <div>Page</div>
-  )
+  return <div className="bg-white">
+    <div className="mx-auto max-w-2xl px-4 pb-24 pt-16 sm:px-6 lg:max-w-7xl lg:px-8">
+    <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl ">Shopping Cart</h1>
+    <div className="mt-12 "></div>
+    </div>
+  </div>
 }
