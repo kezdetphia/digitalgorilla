9440bdefd081f1a76bbb8c7f34e7e50cd5f0ea7f
kezdetphia <fehermark88@gmail.com>
2024-01-02 11:01:25 -0800

    deploy 7 -providers route

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,9 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn, constructMetadata } from "@/lib/utils";
 import Navbar from "@/components/Navbar";
-import Providers from '../components/Providers'
 import { Toaster } from "sonner";
 import Footer from "@/components/Footer";
+import Providers from "@/components/Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
