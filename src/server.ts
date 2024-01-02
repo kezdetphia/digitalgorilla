623e27b27ec7a941ac83948404ffd468eec0aebf
kezdetphia <fehermark88@gmail.com>
2024-01-01 19:32:59 -0800

    config server for security

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import { redirect } from "next/navigation";
 import { getPayloadClient } from "./get-payload";
 import express from "express";
 import { nextApp, nextHandler } from "./next-utils";
@@ -9,6 +10,8 @@ import { stripeWebhookHandler } from "./webhooks";
 import { IncomingMessage } from "http";
 import nextBuild from "next/dist/build";
 import path from "path";
+import { PayloadRequest } from "payload/types";
+import { parse } from "url";
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
@@ -42,6 +45,22 @@ const start = async () => {
     },
   });
 
+  const cartRouter = express.Router();
+
+  cartRouter.use(payload.authenticate);
+
+  cartRouter.get("/", (req, res) => {
+    const request = req as PayloadRequest;
+
+    if (!request.user) return res.redirect("/sign-in?origin=cart");
+
+    const parsedUrl = parse(req.url, true);
+
+    return nextApp.render(req, res, "/cart", parsedUrl.query);
+  });
+
+  app.use("/cart", cartRouter);
+  
   if (process.env.NEXT_BUILD) {
     app.listen(PORT, async () => {
       payload.logger.info("Next.js is building for production");
